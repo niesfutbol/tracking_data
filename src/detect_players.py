0ddf9c21b4cf873a7adf50daba9fd1d78e93d995
Nepo Rojas <braulio.rojas@islas.org.mx>
2024-05-19 00:21:16 -0700

    🚧 Add first version of detector

---
+++ src/detect_players.py
@@ -0,0 +1,22 @@
+from ultralytics import YOLO
+import shutil
+
+# Use custom trained model and predict results
+model = YOLO("yolov8n.pt")
+
+#Define the location of the database
+source = "/workdir/tests/data/"
+output = "/workdir/results/"
+
+#Configure the prediction
+resultsDetect = model.predict(source, conf=0.5, stream=True, show=False, save=True)
+
+#Creates bounding boxes
+for result in resultsDetect:
+    boxes = result.boxes
+    masks = result.masks
+    keypoints = result.keypoints
+    probs = result.probs
+    output_path = result.save_dir + "/" + result.path.split("/")[-1]
+    if "person" in result.verbose():
+        shutil.copy(output_path, output)
